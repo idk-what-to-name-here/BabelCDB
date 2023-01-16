0edadcdbeb3803e0ec91143996336cef05c5a785
Beetron-1 Beetletop <113409293+Beetletop@users.noreply.github.com>
2023-01-16 22:29:37 +0300

    Update c511027692.lua

--- c511027692.lua
+++ c511027692.lua
@@ -6,7 +6,7 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--ha
+	--Prevent damage
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_BE_BATTLE_TARGET)
